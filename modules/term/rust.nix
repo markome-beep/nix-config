e5dccb652cb5752f786f2ce84cf3dd9e8a3ebc9a
Pirro Marko <guyofsoccer@gmail.com>
2025-06-01 00:20:47 -0400

    131 current  2025-06-01 00:20:43  25.11.20250528.96ec055  6.12.30                          *

--- modules/term/rust.nix
+++ modules/term/rust.nix
@@ -4,5 +4,19 @@
     cargo
     gcc
     cargo-tauri
+    pkg-config
+    gobject-introspection
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
   ];
 }
